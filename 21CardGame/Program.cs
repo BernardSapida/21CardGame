6ffe68c56b2f40b31d8b32b08e8388623377447a
BernardSapida <BernardSapida1706@gmail.com>
2022-11-19 20:28:34 +0800

    Stella Done

--- 21CardGame/Program.cs
+++ 21CardGame/Program.cs
@@ -6,6 +6,7 @@
         Deck deck = new Deck();
         Hand playerHand = new Hand();
         Hand computerHand = new Hand();
+        Boolean haveWinner = false;
         static String drawCard = "";
 
         static void Main(string[] args)
@@ -18,6 +19,19 @@
         public void startGame()
         {
             firstDrawCard();
+
+            while(!haveWinner)
+            {
+                playerTurns();
+                computerTurns();
+                Console.WriteLine("\n=====================================\n");
+
+                if (playerHand.getHandValue() >= 21 && computerHand.getHandValue() >= 21)
+                {
+                    checkWinner();
+                    break;
+                }
+            }
         }
 
         public void firstDrawCard()
@@ -28,17 +42,9 @@
             draw("player");
             draw("player");
             Console.WriteLine("Total hand value is " + playerHand.getHandValue());
-            Console.WriteLine("\n=====================================\n");
 
             draw("computer");
             draw("computer");
-
-            while (true)
-            {
-                if(!playerTurns()) break;
-                Console.WriteLine("\n=====================================\n");
-                if (computerHand.getHandValue() < 21) computerTurns();
-            }
         }
 
         public String queryChoice()
@@ -50,7 +56,6 @@
             {
                 Console.WriteLine("Your answer is not accepted!");
                 Console.WriteLine("\n=====================================\n");
-
                 Console.Write("Player, would you like to draw or pass?\n[1] Draw\n[2] Pass\nChoice: ");
                 choice = Console.ReadLine();
             }
@@ -88,7 +93,7 @@
         {
             Random random = new Random();
 
-            if((random.NextInt64(1, 3) == 1 && computerHand.getHandValue() != 21) || playerHand.getHandValue() > 21)
+            if(((random.NextInt64(1, 3) == 1 && computerHand.getHandValue() != 21) && computerHand.getHandValue() < 21) || (playerHand.getHandValue() > 21 && computerHand.getHandValue() < 21) || computerHand.getHandValue() < 21)
             {
                 draw("computer");
                 Console.WriteLine("Computer drew a card.");
@@ -96,36 +101,8 @@
             else pass("computer");
         }
 
-        public Boolean playerTurns()
+        public void playerTurns()
         {
-            if (playerHand.getHandValue() > 21 && computerHand.getHandValue() > 21)
-            {
-                Console.WriteLine("Hand of Player with a value of " + playerHand.getHandValue());
-                card.displayCards("player");
-                Console.WriteLine("\n------ VS ------\n");
-                Console.WriteLine("Hand of Computer with a value of " + computerHand.getHandValue());
-                card.displayCards("computer");
-                Console.WriteLine("\n=====================================\n");
-                Console.WriteLine("You lose! Try again next time!");
-                return false;
-            }
-            else if (playerHand.getHandValue() == 21 && computerHand.getHandValue() == 21)
-            {
-                Console.WriteLine("Game ends in a draw!");
-                return false;
-            }
-            else if (playerHand.getHandValue() == 21 && computerHand.getHandValue() > 21)
-            {
-                Console.WriteLine("Hand of Player with a value of " + playerHand.getHandValue());
-                card.displayCards("player");
-                Console.WriteLine("\n\n------ VS ------\n\n");
-                Console.WriteLine("Hand of Computer with a value of " + computerHand.getHandValue());
-                card.displayCards("computer");
-                Console.WriteLine("\n=====================================\n");
-                Console.WriteLine("Congratulations! You win!");
-                return false;
-            }
-
             if (playerHand.getHandValue() <= 21)
             {
                 if (queryChoice().Equals("1"))
@@ -134,9 +111,38 @@
                     Console.WriteLine("Total hand value is " + playerHand.getHandValue());
                 }
             }
-            else Console.WriteLine("No hope for victory");
+            else
+            {
+                Console.WriteLine("No hope for victory");
+                Console.WriteLine("\n=====================================\n");
+            }
 
-            return true;
+        }
+
+        public void checkWinner()
+        {
+            if((playerHand.getHandValue() > 21 && computerHand.getHandValue() > 21) || (playerHand.getHandValue() > 21 && computerHand.getHandValue() == 21))
+            {
+                Console.WriteLine("Hand of Player with a value of " + playerHand.getHandValue());
+                card.displayCards("player");
+                Console.WriteLine("\n------------------ VS ------------------\n");
+                Console.WriteLine("Hand of Computer with a value of " + computerHand.getHandValue());
+                card.displayCards("computer");
+                Console.WriteLine("You lose! Try again next time!");
+            }
+            else if (playerHand.getHandValue() == 21 && computerHand.getHandValue() == 21)
+            {
+                Console.WriteLine("Game ends in a draw!");
+            }
+            else if (playerHand.getHandValue() == 21 && computerHand.getHandValue() > 21)
+            {
+                Console.WriteLine("Hand of Player with a value of " + playerHand.getHandValue());
+                card.displayCards("player");
+                Console.WriteLine("\n------------------ VS ------------------\n");
+                Console.WriteLine("Hand of Computer with a value of " + computerHand.getHandValue());
+                card.displayCards("computer");
+                Console.WriteLine("Congratulations! You win!");
+            }
         }
     }
 }
